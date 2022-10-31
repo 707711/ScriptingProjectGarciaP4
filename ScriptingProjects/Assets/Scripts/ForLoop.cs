e239da3e25c4bc28cccd0d8693c79594fe62ad2a
707711 <707711@student.dusd.net>
2022-10-31 11:43:59 -0700

    Loops Statements
    on foreach loop

---
+++ ScriptingProjects/Assets/Scripts/ForLoop.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+    int numEnemies = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < nunEnemies; i++)
+        {
+            Debug.Log("Creating enmy number: " + i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
