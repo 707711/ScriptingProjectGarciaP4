feaa5c04a9870f3e95d6b60b990d6d5f4d705876
707711 <707711@student.dusd.net>
2023-01-31 11:22:04 -0800

    describe
    How Mathf.Lerp works

---
+++ ScriptingProjects/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    // the lerp function finds values that is percetage of two numbers to get a number in between
+        float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+    //these work in the same way as Mathf.Lerp but from and to.The functions result in finding a color that is in between and the vector is a percetage of the two numbers
+       Vector3 from =new Vector3 (1f, 2f, 3f);
+       Vector3 to = new Vector3 (5f, 6f, 7f);
+       
+    //These go from 1 to 5, 2 to 6, and from 4 to 7. from finding the 75tgh percent between the num,bers the ruslts are (4, 5, 6)
+       Vector3 result = Vector3.Lerp (from, to, 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    // It smooths the value over time
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+
+    //Over some several frames the value will keep increasing around the value of 8
+       light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    */
+}
