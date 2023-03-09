04f1e4c9d91fbbf1b5dcbd5331b350d2e389a4c1
707711 <707711@student.dusd.net>
2023-03-09 11:03:15 -0800

    mouse click

---
+++ ScriptingProjects/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        rb.AddForce(-transform.forward * 500f);
+        rb.useGravity = true;
+    }
+}
