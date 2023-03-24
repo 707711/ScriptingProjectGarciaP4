ae6f33d00f886568b1ab0f9ccf70ddac24711288
707711 <707711@student.dusd.net>
2023-03-24 10:38:02 -0700

    data types

---
+++ ScriptingProjects/Assets/Scripts/DataTypeScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTypeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vaule type variables
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2, 0);
+
+        //Reference type variable
+        Transform tran = transform;
+        tran.position = new Vector3(0, 0, -5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
